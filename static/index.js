a84a0c9d9555e37ca7ee7cd9e11bb97696a9f24a
unknown <meperezcuello@wpi.edu>
2020-01-18 16:01:58 -0500

    Add cube as background

--- static/index.js
+++ static/index.js
@@ -1,10 +1,13 @@
 const { Scene, PerspectiveCamera, Geometry, LineBasicMaterial, WireframeGeometry, DirectionalLight, OrbitControls, Color, AmbientLight, WebGLRenderer, BoxGeometry,
-	MeshBasicMaterial, LineSegments, Mesh, OBJLoader, LoadingManager} = THREE 
+	MeshBasicMaterial, LineSegments, Mesh, OBJLoader, LoadingManager, Fog, BoxBufferGeometry, MeshNormalMaterial} = THREE 
 
 let scene = new Scene();
-let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+scene.background = new Color( 0xffffff );
+scene.fog = new Fog(0xffffff, 0.2, 10000)
+let camera = new PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 1, 10000 );
 
 let renderer = new WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
 
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -13,23 +16,37 @@ controls.enableZoom = true;
 
 
 
-var keyLight = new DirectionalLight(new Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+let geometry = new BoxBufferGeometry( 100, 100, 100 );
+let material = new MeshNormalMaterial();
 
-var fillLight = new DirectionalLight(new Color('hsl(240, 100%, 75%)'), 1.0);
-fillLight.position.set(100, 0, 100);
+let cubeMaterials = [ 
+    new THREE.MeshBasicMaterial({color:0x2b2d2f, transparent:true, opacity:1, side: THREE.DoubleSide}),
+    new THREE.MeshBasicMaterial({color:0x898989, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({color:0xd3d3d3, transparent:true, opacity:1, side: THREE.DoubleSide}),
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({color:0xaaafaa, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({color:0xbfff00, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+]; 
+var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials); 
 
-var backLight = new DirectionalLight(0x000000, 1);
-backLight.position.set(100, 0, -100).normalize();
 
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
+group = new THREE.Group();
 
+for ( var i = 0; i < 1000; i ++ ) {
 
+	var mesh = new THREE.Mesh( geometry, cubeMaterial );
+	mesh.position.x = Math.random() * 2000 - 1000;
+	mesh.position.y = Math.random() * 2000 - 1000;
+	mesh.position.z = Math.random() * 2000 - 1000;
+	mesh.rotation.x = Math.random() * 2 * Math.PI;
+	mesh.rotation.y = Math.random() * 2 * Math.PI;
+	mesh.matrixAutoUpdate = false;
+	mesh.updateMatrix();
+	group.add( mesh );
 
-// var light = new THREE.AmbientLight( 0x000000 ); // soft white light
-// scene.add( light );
+}
+
+scene.add( group );
 
 
 renderer.setSize(window.innerWidth, window.innerHeight); // need to improve
@@ -39,49 +56,8 @@ window.addEventListener('resize', function() {
 
 document.body.appendChild( renderer.domElement );
 
-let manager = new LoadingManager();
-let loader = new OBJLoader(manager);
-var dolphinOBJ; // global reference for dolphin
-loader.setPath('static/models/');
-loader.load('10014_dolphin_v2_max2011_it2.obj', function(object){
 
-	// object.traverse( function ( child ) {
-
-	// if ( child.isMesh ) {
-
-	// 	child.geometry = new Geometry().fromBufferGeometry( child.geometry );
-	// 	console.log(geometry.vertices)
-
-	// 	// var wireframeGeomtry = new WireframeGeometry( child.geometry );
-	// 	// var wireframeMaterial = new LineBasicMaterial( { color: 0xffffff } );
-	// 	// var wireframe = new LineSegments( wireframeGeomtry, wireframeMaterial );
-	// 	// child.add(wireframe);
-
-	// 	}
-	// });
-
-	object.position.y -= 10;
-	object.rotation.x += 20;
-	object.rotation.y += 10;
-	object.rotateZ( Math.PI  );
-	// console.log(object.attributes.position)
-	dolphinOBJ = object;
-	scene.add(object); 
-
-
-
-});
-
-
-
-
-// let geometry = new BoxGeometry( 1, 1, 1 );
-// let material = new MeshBasicMaterial( { color: 0x00ff00 } );
-// let cube = new Mesh( geometry, material );
-// scene.add( cube );
-camera.position.z -= 250;
-// console.log(dolphinOBJ.attributes.position)
-// camera.position.z = 5;
+camera.position.z -= 1150;
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -90,10 +66,14 @@ function getRandomInt(min, max) {
 let animate = function () {
 	requestAnimationFrame( animate );
 	controls.update();
-	// dolphinOBJ.attributes.position.needsUpdate = true; 
+	var time = Date.now() * 0.0001;
+	let rx = Math.sin( time * 0.7 ) * 0.5;
+	let ry = Math.cos( time * 0.3 ) * 0.5;
+	let rz = Math.sin( time * 0.2 ) * 0.5;
 
-	dolphinOBJ.rotation.z += 0.01;
-	// dolphinOBJ.rotation.z += 0.01;
+	group.rotation.x = rx;
+	group.rotation.y = ry;
+	group.rotation.z = rz;
 
 	renderer.render( scene, camera );
 };
