030851e598e8f072e4fa0f96047b2765df5d5379
unknown <meperezcuello@wpi.edu>
2020-01-17 22:34:28 -0500

    Update 0.05: added joses working files

--- static/index.js
+++ static/index.js
@@ -1,9 +1,10 @@
-const { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, OBJLoader} = THREE 
+const { Scene, PerspectiveCamera,Color, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, OBJLoader, LoadingManager} = THREE 
 
 let scene = new Scene();
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-let renderer = new WebGLRenderer();
+let renderer = new WebGLRenderer({alpha: true});
+
 renderer.setSize(window.innerWidth, window.innerHeight); // need to improve
 window.addEventListener('resize', function() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -12,30 +13,45 @@ window.addEventListener('resize', function() {
 document.body.appendChild( renderer.domElement );
 
 
-let loader = OBJLoader();
 
 
-loader.load(
-	// resource URL
-	'/static/models/10014_dolphin_v2_max2011_it2.obj',
-	// called when resource is loaded
-	function ( object ) {
-		scene.add( object );
-	},
-	// called when loading is in progresses
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	// called when loading has errors
-	function ( error ) {
-		console.log( 'An error happened' );
-	}
-);
 
-let geometry = new BoxGeometry( 1, 1, 1 );
-let material = new MeshBasicMaterial( { color: 0x00ff00 } );
-let cube = new Mesh( geometry, material );
-scene.add( cube );
+var loadOBJ = function() {
+  //Manager from ThreeJs to track a loader and its status
+  var manager = new LoadingManager();
+  //Loader for Obj from Three.js
+  var loader = new OBJLoader(manager);
+
+  //Launch loading of the obj file, addBananaInScene is the callback when it's ready 
+  loader.load('/static/models/10014_dolphin_v2_max2011_it2.obj', function(object) {
+    banana = object;
+    //Move the banana in the scene
+    banana.rotation.x = Math.PI / 2;
+    banana.position.y = -200;
+    banana.position.z = 50;
+    //Go through all children of the loaded object and search for a Mesh
+    object.traverse(function(child) {
+      //This allow us to check if the children is an instance of the Mesh constructor
+      if (child instanceof Mesh) {
+        child.material.color = new Color(0X00FF00);
+        //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
+        child.geometry.computeVertexNormals();
+      }
+    });
+    
+    scene.add(banana);
+    // renderer.render();
+  });
+};
+
+loadOBJ();
+
+
+
+// let geometry = new BoxGeometry( 1, 1, 1 );
+// let material = new MeshBasicMaterial( { color: 0x00ff00 } );
+// let cube = new Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
 
@@ -63,8 +79,8 @@ let animate = function () {
 	// resizeCanvasToDisplaySize();
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 };
